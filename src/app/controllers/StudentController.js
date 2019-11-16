c1cf3286ca87451dcc242872ca2202a3a3ea5608
Bruno Amaral Futema <bruno.futema@outlook.com>
2019-11-16 16:29:02 -0300

    feature: register student

---
+++ src/app/controllers/StudentController.js
@@ -0,0 +1,57 @@
+import * as Yup from 'yup';
+
+import User from '../models/User';
+import Student from '../models/Student';
+
+class StudentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      age: Yup.required(),
+      weight: Yup.required(),
+      height: Yup.required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res
+        .status(401)
+        .json({ error: 'Falha na validação, verifique seus dados!' });
+    }
+
+    const user = await User.findByPk(req.userId);
+
+    if (!user.administrator) {
+      return res.status(401).json({
+        error: 'Apenas usuários administradores podem cadastrar os alunos!',
+      });
+    }
+
+    const { email } = req.body;
+
+    const studentExists = await Student.findOne({ where: { email } });
+
+    if (studentExists) {
+      return res
+        .status(401)
+        .json({ error: 'Este estudante já foi cadastrado!' });
+    }
+
+    const { id, name, age, weight, height } = await Student.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      age,
+      weight,
+      height,
+    });
+  }
+
+  async update(req, res) {
+    return res.json({ ok: true });
+  }
+}
+
+export default new StudentController();
