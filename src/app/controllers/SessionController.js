d3324b99985a2f4bb0b4f84be20a18235eb75fcf
Bruno Amaral Futema <bruno.futema@outlook.com>
2019-11-16 01:19:53 -0300

    feature: jwt token in session controller

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,4 +1,7 @@
+import jwt from 'jsonwebtoken';
 import * as Yup from 'yup';
+
+import authConfig from '../../config/auth';
 
 import User from '../models/User';
 
@@ -32,9 +35,14 @@ class SessionController {
     const { id, name } = user;
 
     return res.json({
-      id,
-      name,
-      email,
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
     });
   }
 }
